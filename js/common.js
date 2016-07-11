45fcdb02031f1de12d50311e88857850ff53eb77
alex <sania_free@i.ua>
2016-07-11 16:43:23 +0300

    responsive

--- js/common.js
+++ js/common.js
@@ -1,24 +1,15 @@
-$(function(){
+$(document).ready(function() {
+	//fancybox
 	$('.fancybox').fancybox({
-		padding: 0,
-		margin: 10,
-		openEffect: 'fade',
-		closeEffect: 'fade',
-		openSpeed: 400,
-		closeSpeed: 400,
 		helpers: {
 			overlay: {
 				locked: false
 			}
 		}
 	});
-
-
-	//------------------    menu scroll    -------------------------------
-
-	/*
-	$('.top_menu li a.subNavBtn').on('click',function(){
-		$('html,body').animate({scrollTop:$($(this).attr('href')).offset().top-30},800);
-		return false;
+	
+	//toggle menu
+	$( ".menu_trigger" ).click(function() {
+	  $( this ).next('ul').slideToggle('fast');
 	});
-	*/
+});
