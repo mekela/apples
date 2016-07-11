77efe90d13eb0b453299be7faadfa6cf76d15cfa
Serdiuk Serhii <sera.serduk@gmail.com>
2016-07-11 14:49:57 +0300

    add 3 pages
    404
    product-cart

--- js/common.js
+++ js/common.js
@@ -13,6 +13,19 @@ $(function(){
 		}
 	});
 
+	// falling_sakura
+	if($(".falling_sakura").length) {
+		$('.falling_sakura').sakura({ 
+			blows: ['blow-soft-left', 'blow-medium-left', 'blow-hard-left', 'blow-soft-right', 'blow-medium-right', 'blow-hard-right'], // Horizontal movement animation names
+			className: 'sakura', // Class name to use
+			fallSpeed: 1, // Factor for petal fall speed
+			maxSize: 24, // Maximum petal size
+			minSize: 14, // Minimum petal size
+			newOn: 300, // Interval after which a new petal is added
+			swayAnimations: ['sway-0', 'sway-1', 'sway-2', 'sway-3', 'sway-4', 'sway-5', 'sway-6', 'sway-7', 'sway-8'] // Swaying animation names
+		});
+	}	
+});
 
 	//------------------    menu scroll    -------------------------------
 
